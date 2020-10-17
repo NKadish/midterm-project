92a4517f0cb0968df01e5aa8abe34314e0fddc4a
Jason Long <jason.long77@hotmail.com>
2020-10-17 17:27:39 +0000

    Added Menu router and route

---
+++ routes/menu.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM menu_items;`)
+      .then(data => {
+        const items = data.rows;
+        res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
