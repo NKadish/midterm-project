47b0b1458354178d4f0ad9811732120b3fcb909e
jennyg0 <jen2348@hotmail.com>
2020-10-18 00:31:08 +0000

    update header and register page

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
 
   const loadMenu = function () {
+    $.ajax({
+      url: "/menu",
+      method: "GET"
+    }) .then ((food) => {
+      getAllMenu(food);
+    })
+  };
 
-  }
+  loadMenu();
 
+  // const renderMenu = function(menu) {
+  //   for (const food of menu) {
+  //     const $output = createMenu(food);
+  //     $('#menu-items').prepend($output);
+  //   }
+  //  };
 
-  const renderMenu = function(menu) {
-    for (const food of menu) {
-      const $output = createMenu(food);
-      $('#menu-items').prepend($output);
-    }
-   };
+   const createFoodItem = function(item) {
+     const $food = $(`<article
 
+     `
+
+     )
+   }
 
 
 
