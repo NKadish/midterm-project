59ead7283d3b13debbc9ef5646cd49bd83881349
Jason Long <jason.long77@hotmail.com>
2020-10-18 02:45:30 +0000

    Removed comments

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,3 @@
-/*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
 
 const express = require('express');
 const router  = express.Router();
