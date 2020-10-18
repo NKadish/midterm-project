c18f8f34315cb4f081673d983b3b6404740ff4aa
NKadish <noahkadish@hotmail.com>
2020-10-18 18:57:53 +0000

    add password to users db

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,5 +5,6 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(255) NOT NULL
+  phone_number VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, phone_number)
-VALUES ('Alice', 'alice@alice.com', '1234567890');
-INSERT INTO users (name, email, phone_number)
-VALUES ('Noah', 'noahkadish@hotmail.com', '1016761010');
-INSERT INTO users (name, email, phone_number)
-VALUES ('Megaman', 'megaman@hotmail.com', '1016761018');
-INSERT INTO users (name, email, phone_number)
-VALUES('Jason', 'jason.long77@hotmail.com', '9999999989');
-INSERT INTO users (name, email, phone_number)
-VALUES('Bryan', 'bryan@hotmail.com', '9999999999');
+INSERT INTO users (name, email, phone_number, password)
+VALUES ('Alice', 'alice@alice.com', '1234567890', 'password');
+INSERT INTO users (name, email, phone_number, password)
+VALUES ('Noah', 'noahkadish@hotmail.com', '1016761010', 'password');
+INSERT INTO users (name, email, phone_number, password)
+VALUES ('Megaman', 'megaman@hotmail.com', '1016761018', 'password');
+INSERT INTO users (name, email, phone_number, password)
+VALUES('Jason', 'jason.long77@hotmail.com', '9999999989', 'password');
+INSERT INTO users (name, email, phone_number, password)
+VALUES('Bryan', 'bryan@hotmail.com', '9999999999', 'password');
