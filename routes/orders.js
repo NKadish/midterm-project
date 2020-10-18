f143c088f597de3db3e57113996f6f17960d7a8d
jennyg0 <jen2348@hotmail.com>
2020-10-18 21:18:44 +0000

    add order and menu routes

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  router.get("/", (req, res) => {
+    res.render("orders");
+  });
+
   router.get("/", (req, res) => {
     let query = `SELECT * FROM orders`;
     db.query(query)
