f3d41e73aabb87f2eddfa0ebeb452b370b9d4597
Jason Long <jason.long77@hotmail.com>
2020-10-19 17:04:55 +0000

    Sending db data to templateVars

--- routes/menu.js
+++ routes/menu.js
@@ -4,24 +4,27 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-  // router.get("/", (req, res) => {
-  //   const templateVars = { user : req.session.id };
-  //   res.render("menu", templateVars);
-  // });
-
   router.get("/", (req, res) => {
     return getAllMenu()
-      // .then(data => {
-      //   res.render("menu", templateVar);
-      // })
       .then(data => {
-        res.send({ data });
+        const templateVars = { user : req.session.id, data };
+        console.log(templateVars);
+        res.render("menu", templateVars);
       })
+      // .then(data => {
+      //   res.send({ data });
+      // })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
   });
+
+
+  // router.get("/", (req, res) => {
+  //   res.render("menu", templateVars);
+  // });
+
   return router;
 };
