257ebd48f3c9389cea630ccc827c90037343c4c4
NKadish <noahkadish@hotmail.com>
2020-10-19 20:15:20 +0000

    fix users seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, phone_number, password)
-VALUES ('Alice', 'alice@alice.com', '1234567890', 'password');
-VALUES ('Noah', 'noahkadish@hotmail.com', '1016761010', 'password'),
-VALUES ('Megaman', 'megaman@hotmail.com', '1016761018', 'password'),
-VALUES('Jason', 'jason.long77@hotmail.com', '9999999989', 'password'),
-VALUES('Bryan', 'bryan@hotmail.com', '9999999999', 'password');
+VALUES ('Alice', 'alice@alice.com', '1234567890', 'password'),
+('Noah', 'noahkadish@hotmail.com', '1016761010', 'password'),
+('Megaman', 'megaman@hotmail.com', '1016761018', 'password'),
+('Jason', 'jason.long77@hotmail.com', '9999999989', 'password'),
+('Bryan', 'bryan@hotmail.com', '9999999999', 'password');
