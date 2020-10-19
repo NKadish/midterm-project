c45eb3c4a033c8a544fc42857ca1372caa22efd5
Jason Long <jason.long77@hotmail.com>
2020-10-19 16:56:16 +0000

    Revised menu route

--- routes/menu.js
+++ routes/menu.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const { getAllMenu } = require('../server/database');
 const router  = express.Router();
 
 module.exports = (db) => {
 
-  router.get("/", (req, res) => {
-    const templateVars = { user : req.session.id };
-    res.render("menu", templateVars);
-  });
+  // router.get("/", (req, res) => {
+  //   const templateVars = { user : req.session.id };
+  //   res.render("menu", templateVars);
+  // });
 
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM menu_items;`)
+    return getAllMenu()
+      // .then(data => {
+      //   res.render("menu", templateVar);
+      // })
       .then(data => {
-        const items = data.rows;
-        res.json({ items });
+        res.send({ data });
       })
       .catch(err => {
         res
