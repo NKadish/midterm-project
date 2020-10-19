4fe71eeb8b25b35f62f3c23ad4af55514f000662
NKadish <noahkadish@hotmail.com>
2020-10-19 20:17:19 +0000

    add order for user 1

--- db/seeds/04_orders.sql
+++ db/seeds/04_orders.sql
@@ -1,6 +1,7 @@
 INSERT INTO orders (user_id, restaurant_id, placed_at, picked_up_at, status)
 VALUES (1, 1, '2016-08-08 12:12:12', '2016-08-08 12:35:12', false),
 (2, 1, '2017-09-09 11:11:11', '2017-09-09 11:20:11', false),
+(1, 1, '2016-08-08 12:12:12', '2016-08-08 12:35:12', false),
 (1, 1, null, null, true);
 
 
