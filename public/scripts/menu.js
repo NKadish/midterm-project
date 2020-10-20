d811c0c84e3bf11e1416eeda96a91c34ef2ff3c0
NKadish <noahkadish@hotmail.com>
2020-10-20 00:06:19 +0000

    Add shopping cart functionality

---
+++ public/scripts/menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('helloworld')
+  $('.addToCart').click(function(event) {
+    event.preventDefault();
+    let $menuItemId = $(this).val();
+    console.log($menuItemId);
+    let $quantity = 1;
+    $.ajax({
+      url: '/menu',
+      method: 'POST',
+      data: {$menuItemId, $quantity}
+    });
+  });
+});
