3b753d3514dbd06cf048d793da821e1a16276cc1
NKadish <noahkadish@hotmail.com>
2020-10-22 15:20:21 +0000

    add ajax so that when an item is added to the cart a little scrolldown happens that says added to cart

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  console.log('helloworld')
+  $('.added-popup').hide();
   $('.addToCart').click(function(event) {
     event.preventDefault();
     let $menuItemId = $(this).val();
-    console.log($menuItemId);
     let $quantity = ($(this).parent().find('.quantity')).val();
-    console.log($quantity);
+    let addedMsg = '';
+    addedMsg += '<b> Added to the cart! </b>'
+    $('.added-popup').hide();
+    $('.added-popup').empty();
+    ($(this).parent().find('.added-popup')).append(addedMsg);
+    ($(this).parent().find('.added-popup')).slideDown('slow');
     $.ajax({
       url: '/menu',
       method: 'POST',
