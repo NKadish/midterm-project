bdb6a19eb7a5547bf7383f35f25a01395590f187
NKadish <noahkadish@hotmail.com>
2020-10-23 00:07:28 +0000

    fix some menu names

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,24 +1,24 @@
 INSERT INTO menu_items (name, description, cost, time_to_make, picture_url)
 VALUES('Big Wac', 'Our signature sandwich! Warning: it might get messy! Comes with a drink and fries.', 1039, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-big-mac.jpg'),
-('Quarter Pound Burger with Cheese', 'For those who want a little more beef. Comes with a drink and fries.', 1100, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-quarter-pounder-cheese.jpg'),
+('Quarter Pound', 'For those who want a little more beef. Comes with a drink and fries.', 1100, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-quarter-pounder-cheese.jpg'),
 ('WcChicken', 'Our signature chicken sandwich. Comes with a drink and fries.', 1039, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-mcchicken.jpg?'),
 ('WcDouble', 'A smaller beef burger', 309, 15, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-mcdouble.jpg'),
 ('Tiny Chicken', 'A smaller chicken burger', 269, 15, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-junior-chicken.jpg'),
 ('Hamburger', 'For those who just want a simple burger', 169, 15, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-hamburger.jpg'),
-('10 Chicken Nuggets', '10 piece chicken nuggets! Comes with a sauce, a drink and fries.', 1299, 25, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-10-chicken-mcnuggets.jpg'),
+('10 Nuggets', '10 piece chicken nuggets! Comes with a sauce, a drink and fries.', 1299, 25, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-10-chicken-mcnuggets.jpg'),
 ('Chicken Wrap', 'A chicken wrap with lettuce, cheese, bacon and ranch. Comes with a drink and fries.', 769, 10, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-chicken-bacon-signature-mcwrap-crispy.jpg'),
-('Fried Fish Sandwich', 'Fried fish patty with tartar sauce and a half slice of cheese! Comes with a drink and fries.', 1039, 15, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-filet-o-fish.jpg'),
+('Fried Fish', 'Fried fish patty with tartar sauce and a half slice of cheese! Comes with a drink and fries.', 1039, 15, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-filet-o-fish.jpg'),
 ('Fries', 'Our famous fries!', 299, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-fries-medium.jpg'),
 ('Egg WcMuffin', 'A breakfast sandwich which has ham, egg and cheese', 499, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-egg-mcmuffin.jpg'),
-('Bacon and Egg WcMuffin', 'Our breakfast sandwich with bacon instead of ham', 499, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-bacon-n-egg-mcmuffin.jpg'),
-('Sausage and Egg WcMuffin', 'Our breakfast sandwich with sausage instead of ham', 499, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-sausage-n-egg-mcmuffin.jpg'),
+('Bacon WcMuffin', 'Our breakfast sandwich with bacon instead of ham', 499, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-bacon-n-egg-mcmuffin.jpg'),
+('Sausage WcMuffin', 'Our breakfast sandwich with sausage instead of ham', 499, 20, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-sausage-n-egg-mcmuffin.jpg'),
 ('Pancakes and Sausage', 'Pancakes and sausages! Perfect for a breakfast', 529, 25, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-hotcakes-syrup-butter.jpg'),
 ('Coffee', 'Better than Tom Hurtins coffee', 169, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-coffee.jpg'),
 ('Tea', 'For when you need to relax', 169, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-higgins-burke-orange-pekoe-tea.jpg'),
 ('Muffin', 'Our signature muffin!', 169, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-blueberry-muffin.jpg'),
 ('Apple Pie', 'Our signature apple pies!', 129, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-baked-apple-pie.jpg'),
 ('WcFlurry', 'Some ice cream for your sweet tooth', 359, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-oreo-mcflurry.jpg'),
-('Chocolate Milkshake', 'The milk is shooketh', 359, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-chocolate-milkshake.jpg?');
+('Milkshake', 'The milk is shooketh', 359, 5, 'https://www.mcdonalds.com/is/image/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-chocolate-milkshake.jpg?');
 
 
 
